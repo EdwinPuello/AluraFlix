97d0560da0703bc489c5fd9c8552622d9d403000
Edwin Puello <eduwinpuello@worldoffice.com.co>
2025-01-09 18:44:37 -0500

    replace BrowserRouter with HashRouter for improved routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import NuevoVideo from './components/NuevoVideos';
